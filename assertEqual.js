864fc2aefc8caa2ef5587f2ee037cf6e23ac97ef
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-11-24 16:36:55 -0330

    Function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  
+  }
+  
+  if (actual !== expected) {
+    console.log("❌❌❌Assertion failed: [actual] !== [expected]");
+  }
+};
+  
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
