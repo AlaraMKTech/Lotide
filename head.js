2097d0a22f56de1dcf87fd34d8fad081ba58b094
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-15 13:28:06 -0330

    Refactoring

--- head.js
+++ head.js
@@ -1,16 +1,12 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-    
-    }
-    
-    if (actual !== expected) {
-      console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require("./assertEqual");
 
 function head(array) {
-    let firstValue = array[0];
     return array[0];
-};
-    
+}
+
+console.log("Running head.js alone");
+assertEqual(head([1, 2, 3]), 1);
+
+module.exports = head;
+
+   
