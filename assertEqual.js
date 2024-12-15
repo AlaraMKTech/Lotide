1a217050f169ddaea14d7701072a62e79e8e7ece
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-15 13:29:30 -0330

    Refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,5 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}`);
   }
 };
-  
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
