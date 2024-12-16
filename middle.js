8a72bd65b0c1361098894056a16822f05e37f290
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-16 08:57:15 -0330

    Refactoring

--- middle.js
+++ middle.js
@@ -1,32 +1,14 @@
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
-        }
-    }
-    return true;
-}; 
-
-const assertArraysEqual = function(actual, expected) {
-    if (eqArrays(actual, expected)) {
-        console.log(`✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-    } else {
-        console.log(`❌ Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-    }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArrayEqual");
 
 const middle = function(array) {
-  if (array.length <= 2) {
-  return[];  
-  } else if (array.length % 2 === 1) {
-    return[array[Math.floor]] 
-  } else {
-    return [array[Math.floor], array[Math.floor]];
-  }
-};
+    if (array.length <= 2) {
+      return [];
+    } else if (array.length % 2 === 1) {
+      return [array[Math.floor(array.length / 2)]];
+    } else {
+      return [array[Math.floor((array.length - 1) / 2)], array[Math.floor(array.length / 2)]];
+    }
+  };
 
-middle([1, 2, 3, 4, 5]);
+module.exports = middle;
