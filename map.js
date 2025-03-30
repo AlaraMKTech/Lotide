20d12c51d1157cda753039a8b27234ab3c60e13e
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-03-30 17:07:08 -0230

    Edited so that function is exported properly

--- map.js
+++ map.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
-        }
-    }
-    return true;
-}; 
-
-const assertArraysEqual = function(actual, expected) {
-    if (eqArrays(actual, expected)) {
-        console.log(`✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-    } else {
-        console.log(`❌ Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-    }
-};
-
 const map = function (array, callback) {
     const results = [];
     for (let item of array) {
@@ -26,3 +5,5 @@ const map = function (array, callback) {
     }
     return results;
   };
+
+module.exports = map;
