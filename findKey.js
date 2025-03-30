057e930a49d959c8b051e5cc8d071ecab935c08e
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-03-30 17:09:38 -0230

    Edited so that function is exported properly

--- findKey.js
+++ findKey.js
@@ -1,17 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-    
-    }
-    
-    if (actual !== expected) {
-      console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}`);
-    }
-  };
-    
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 function findKey(object, callback) {
     for (let key in object) {
@@ -22,16 +9,4 @@ function findKey(object, callback) {
     return undefined;
 };
 
-const result1 = findKey(
-    {
-      "Blue Hill": { stars: 1 },
-      Akaleri: { stars: 3 },
-      noma: { stars: 2 },
-      elBulli: { stars: 3 },
-      Ora: { stars: 2 },
-      Akelarre: { stars: 3 },
-    },
-    (x) => x.stars === 2
-  );
-
-  assertEqual(result1, "noma"); 
+module.exports = findKey;
