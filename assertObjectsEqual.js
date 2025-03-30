8a8857362779de22a5f69766bd551130da0c8004
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-03-30 17:05:02 -0230

    Edited so that all functions are properly exported

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,23 +11,4 @@ const assertObjectsEqual = function(actual, expected) {
     }
   };
 
-// Test cases
-const shirtObject = { color: 'red', size: 'medium' };
-const anotherShirtObject = { size: 'medium', color: 'red' };
-assertObjectsEqual(shirtObject, anotherShirtObject);
-
-const longSleeveShirtObject = { size: 'medium', color: 'red', sleeveLength: 'long' };
-assertObjectsEqual(shirtObject, longSleeveShirtObject);
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
-
-const object1 = { a: 1, b: [2, 3] };
-const object2 = { a: 1, b: [2, 3] };
-const object3 = { a: 1, b: [3, 2] };
-const object4 = { a: 1, b: 2 };
-assertObjectsEqual(object1, object2);
-assertObjectsEqual(object1, object3);
-assertObjectsEqual(object1, object4);
+module.exports = assertObjectsEqual;
--- takeUntil.js
+++ takeUntil.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return false;
-        }
-    }
-    return true;
-}; 
-
-const assertArraysEqual = function(actual, expected) {
-    if (eqArrays(actual, expected)) {
-        console.log(`✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-    } else {
-        console.log(`❌ Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-    }
-};
-
 const takeUntil = function(array, callback) {
     let result = [];
     for (let i = 0; i < array.length; i++) {
@@ -30,19 +9,4 @@ const takeUntil = function(array, callback) {
     return result;
 };
 
-// Test Code
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-console.log('---');
-
-const data3 = [7, 5, 9, 8, 6, 10, 30, 45, 757];
-const results3 = takeUntil(data3, x => x > 25);
-console.log(results3)
+module.exports = takeUntil;
