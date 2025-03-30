d78be9568d6584b659d87bf38de6527e1f7454aa
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-03-30 17:22:21 -0230

    Edited so that function is exported properly

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+    } else {
+        console.log(`❌ Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+    }
+};
+
+module.exports = assertArraysEqual;
+
+
+
+
+
